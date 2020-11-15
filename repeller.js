d5d939c675f1b7f69463e44736c09bf61bf766a9
yunku12 <70184002+yunku12@users.noreply.github.com>
2020-11-15 22:46:43 +0900

    repeller 방향이 바뀌어있어서 원상복구
    1 ☞ -1

--- repeller.js
+++ repeller.js
@@ -16,7 +16,7 @@ class Repeller {
     let d = dir.mag(); // Distance between objects
     dir.normalize(); // Normalize vector (distance doesn't matter here, we just want this vector for direction)
     d = constrain(d, 1, 100); // Keep distance within a reasonable range
-    let force = 1 * this.power / (d * d); // Repelling force is inversely proportional to distance
+    let force = -1 * this.power / (d * d); // Repelling force is inversely proportional to distance
     dir.mult(force); // Get force vector --> magnitude * direction
     return dir;
   }
